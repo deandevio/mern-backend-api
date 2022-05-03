9260cd89b8520a66b139994b65ba81c106de83f5
deandev <iamdekanos@gmail.com>
2022-05-03 07:38:42 +0300

    back to work, checking the db connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   const db = await mongoose.connect(process.env.DB_URI, {});
-
   console.log(`The mongo db is connected on host ${db.connection.host}`);
 };
 
